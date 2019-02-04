11ee1ac9f4f04e426702795b9f9655a944f44107
Max Houston Ramirez Martel <maxr524@gmail.com>
2019-02-03 23:14:40 -0500

    sumando mas Consultas

--- main.go
+++ main.go
@@ -26,74 +26,73 @@ type Results struct {
 	Heros []Hero `json:"results"`
 }
 type Hero struct {
-	Id          int    `json:"id"`
-	Name        string `json:"name"`
-	Description string `json:"description"`
-	Comic       Comics `json:"comics"`
+	Id          int     `json:"id"`
+	Name        string  `json:"name"`
+	Description string  `json:"description"`
+	Creation    string  `json:"modified"`
+	Serie       Series  `json:"series"`
+	Comic       Comics  `json:"comics"`
+	Storie      Stories `json:"stories"`
 }
 type Comics struct {
+	Items []Item `json:"items"`
+}
+type Series struct {
+	Items []Item_s `json:"items"`
+}
+type Stories struct {
 	Items []Item `json:"items"`
 }
 type Item struct {
 	Name string `json:"name"`
 	Type string `json:"type"`
 }
+type Item_s struct {
+	Name string `json:"name"`
+	Type string `json:"type"`
+}
 
 func main() {
-
+	//Timestamp
 	t := time.Now()
 	ts := t.Format("20060102150405")
-
+	//Creamos el hash
 	hasher := md5.New()
 	hasher.Write([]byte(ts + api_priv + api_key))
+	//Pasamos el hash a texto
 	texto := hex.EncodeToString(hasher.Sum(nil))
-
-	link := "http://gateway.marvel.com/v1/public/characters?ts=" + ts + "&apikey=" + api_key + "&hash=" + texto
-
-	response, err := http.Get(link)
-
-	if err != nil {
-		fmt.Print(err.Error())
-		os.Exit(1)
-	}
-
-	responseData, err := ioutil.ReadAll(response.Body)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	var responseObject Response
-	json.Unmarshal(responseData, &responseObject)
-
+	//Creamos el menu
 	menu :=
 		`
 	Bienvenido
 	
-	Bienvenido
-	Que deseas hacer
+	¿Qué deseas hacer?
+
 	[ 1 ] Personaje
 	[ 2 ] Listarlos
 	`
 	fmt.Print(menu)
 
 	option := 0
+
 	fmt.Scanf("%d", &option)
+
 	switch option {
+
 	case 1:
-		fmt.Println("Que pensonajes deseas ver")
+
+		fmt.Println("¿Qué pensonajes deseas ver?")
 
 		reader := bufio.NewReader(os.Stdin)
 		name, _ := reader.ReadString('\n')
 
 		fmt.Print("Los datos de " + name)
-
+		//pasamos el nombre a modo URL " "="%20"
 		uname := &url.URL{}
 		err := uname.UnmarshalBinary([]byte(name))
 		if err != nil {
 			log.Fatal(err)
 		}
-
 		nombre := uname.String()
 		nom := strings.Replace(nombre, "%0A", "", -1)
 
@@ -116,20 +115,61 @@ func main() {
 		json.Unmarshal(respData, &objeto)
 
 		fmt.Println("*--ID--*")
-		fmt.Println(responseObject.Data.Heros[0].Id)
+		fmt.Println(objeto.Data.Heros[0].Id)
 		fmt.Println("*--Nombre--*")
-		fmt.Println(responseObject.Data.Heros[0].Name)
+		fmt.Println(objeto.Data.Heros[0].Name)
 		fmt.Println("*--Description--*")
-		fmt.Println(responseObject.Data.Heros[0].Description)
+		fmt.Println(objeto.Data.Heros[0].Description)
+		fmt.Println("*--Description--*")
+		fmt.Println(objeto.Data.Heros[0].Creation)
 		fmt.Println("*--Comics--*")
-		cant := len(responseObject.Data.Heros[0].Comic.Items)
+		cant := len(objeto.Data.Heros[0].Comic.Items)
+		fmt.Print("*--Existen ")
+		fmt.Print(cant)
+		fmt.Println(" Comics-*")
 
 		for i := 0; i < cant; i++ {
-			fmt.Println(responseObject.Data.Heros[0].Comic.Items[i].Name)
+			fmt.Println(objeto.Data.Heros[0].Comic.Items[i].Name)
+		}
+		fmt.Println("*--Stories--*")
+		canti := len(objeto.Data.Heros[0].Storie.Items)
+		fmt.Print("*--Existen ")
+		fmt.Print(canti)
+		fmt.Println(" Stories-*")
+		for i := 0; i < canti; i++ {
+			fmt.Println(objeto.Data.Heros[0].Storie.Items[i].Name)
+		}
+		fmt.Println("*--Stories--*")
+		cantid := len(objeto.Data.Heros[0].Serie.Items)
+		fmt.Print("*--Existen ")
+		fmt.Print(cantid)
+		fmt.Println(" Series-*")
+		for i := 0; i < cantid; i++ {
+			fmt.Println(objeto.Data.Heros[0].Serie.Items[i].Name)
 		}
 
 	case 2:
+
+		link := "http://gateway.marvel.com/v1/public/characters?ts=" + ts + "&apikey=" + api_key + "&hash=" + texto
+
+		response, err := http.Get(link)
+
+		if err != nil {
+			fmt.Print(err.Error())
+			os.Exit(1)
+		}
+
+		responseData, err := ioutil.ReadAll(response.Body)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		var responseObject Response
+		json.Unmarshal(responseData, &responseObject)
+
 		fmt.Println("Los personajes de marvel son:")
+
 		for i := 0; i < len(responseObject.Data.Heros); i++ {
 			fmt.Print("ID: ")
 			fmt.Print(responseObject.Data.Heros[i].Id)
